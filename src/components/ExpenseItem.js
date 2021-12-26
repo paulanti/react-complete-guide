2c848c7cd54f00fe3978e8ebe5e90fe411b1a676
Pavel Yansapov <pavel.yansapov@gmail.com>
2021-12-26 14:18:03 +0500

    Use arrow functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,7 +2,7 @@ import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
